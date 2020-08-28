32be75cb884a835c58e86d8563ab6fe5ce3cabfb
dimonise <simtzis@gmail.com>
2020-08-28 15:18:24 +0300

    addition to homework with copecks

--- homework1.py
+++ homework1.py
@@ -1,8 +1,17 @@
 hrn = int(input('Введите количество гривен '))
 kop = int(input('Введите количество копеек '))
+kopString = ''
 
 if kop >= 100:
-    hrn += kop/100
+    hrn += kop / 100
     kop = 0
 
-print(hrn,' гривен',kop,' копеек')
+if kop < 100:
+    if kop == 1:
+        kopString = ' копейка'
+    elif 4 < kop < 21 or 4 < kop % 10 < 10 or kop % 10 == 0:
+        kopString = ' копеек'
+    elif 1 < kop % 10 < 5:
+        kopString = ' копейки'
+
+print(hrn, ' гривен', kop, kopString)

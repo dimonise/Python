2b1ef6266c10f417f377db05f7a1557397a41a1d
dimonise <simtzis@gmail.com>
2020-09-01 19:19:49 +0300

    first classwork

--- homework1.py
+++ homework1.py
@@ -6,12 +6,19 @@ if kop >= 100:
     hrn += kop / 100
     kop = 0
 
+if hrn % 10 == 1 and hrn != 11:
+    hrnString = ' гривна'
+elif 4 < hrn < 21 or 4 < hrn % 10 < 10 or hrn % 10 == 0:
+    hrnString = ' гривен'
+elif 1 < hrn % 10 < 5:
+    hrnString = ' гривны'
+
 if kop < 100:
-    if kop == 1:
+    if kop % 10 == 1 and kop != 11:
         kopString = ' копейка'
     elif 4 < kop < 21 or 4 < kop % 10 < 10 or kop % 10 == 0:
         kopString = ' копеек'
     elif 1 < kop % 10 < 5:
         kopString = ' копейки'
 
-print(hrn, ' гривен', kop, kopString)
+print(hrn, hrnString, kop, kopString)
---
+++ klasswork.py
@@ -0,0 +1,8 @@
+first = int(input('Введите первое число '))
+second = int(input('Введите второе число '))
+result = int(input('Результат умножения первого числа на второе? '))
+res = first * second
+if result == res:
+    print('Ответ верный')
+else:
+    print('Ответ не верный')
